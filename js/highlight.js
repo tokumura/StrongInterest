5815f7de5df4d482e683381749690775da9c3814
Tsuyoshi Okumura <t-okumura@san-ei-med.co.jp>
2015-02-09 18:52:05 +0900

    add button to set interest and remove interest.

--- js/highlight.js
+++ js/highlight.js
@@ -1,12 +1,39 @@
 chrome.storage.local.get(function(items) {
 	var hit_num = 0;
+	var current = window.location.toString();
+	var should_set_badge = false;
+
+	// トップページはバッチテキストをセット
+	if (current == "http://192.168.0.24/cgi-bin6/ag.exe?page=AGIndex") {
+		should_set_badge = true;
+	}
+
+	// トップページはバッチテキストをセット（微妙なURL違い）
+	if (current == "http://192.168.0.24/cgi-bin6/ag.exe") {
+		should_set_badge = true;
+	}
+
+	// 月スケジュールページもバッチセット
+	if (current.indexOf("http://192.168.0.24/cgi-bin6/ag.exe?page=ScheduleUserMonth") >= 0) {
+		should_set_badge = true;
+	}
+
+	// それ以外はバッチセットしない
+	if (!should_set_badge) {
+		chrome.runtime.sendMessage({hit_num: ""}, function(response) {});
+		return;
+	}
+
+	// バッチセット処理
 	var pages = items.pages;
 	var url_arr = pages.split(/\r\n|\r|\n/);
 	for (i = 0; i < url_arr.length; i++) {
 		var url = url_arr[i].replace("http://192.168.0.24/cgi-bin6/", "");
 		$('*[href="' + url + '"]').css({"color":"white" , "background-color":"#e3477e"});
-		var num = $('*[href="' + url + '"]').size();
-		hit_num = hit_num + num;
+		var num = 0;
+		if ($('*[href="' + url + '"]').size() > 0) {
+			hit_num = hit_num + 1;
+		}
 	}
 	chrome.runtime.sendMessage({hit_num: String(hit_num)}, function(response) {});
 });
--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,52 @@
-function changeColor(color) {
-    chrome.tabs.executeScript(null, { file: "js/change.js" });
+/*
+* 気になるリストに追加
+*/
+function addInterest() {
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	    var url = tabs[0].url;
+		chrome.storage.local.get(function(items) {
+			var pages = items.pages;
+			var pages = pages + "\r\n" + url;
+			chrome.storage.local.set({pages:pages});
+			$('*[class="vr_contentArea"]').css({"color":"white" , "background-color":"#e3477e"});
+			window.close();
+		});
+	});
 }
 
-
-document.getElementById('red').onclick = function() {
-    changeColor('red');
+/*
+* 気になるリストから削除
+*/
+function removeInterest() {
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	    var remove_url = tabs[0].url;
+		chrome.storage.local.get(function(items) {
+			var new_pages_arr = new Array();
+			var pages = items.pages;
+			var url_arr = pages.split(/\r\n|\r|\n/);
+			for (i = 0; i < url_arr.length; i++) {
+				var url = url_arr[i];
+				if (url.indexOf(remove_url) < 0) {
+					new_pages_arr.push(url);
+				}
+			}
+			var pages_new = new_pages_arr.join("\r\n");
+			chrome.storage.local.set({pages:pages_new});
+			window.close();
+		});
+	});
 }
 
-document.getElementById('yellow').onclick = function() {
-    changeColor('yellow');
+/*
+* 「気になる」ボタンクリック
+*/
+document.getElementById('add_interest').onclick = function() {
+	addInterest();
+}
+
+/*
+* 「忘れてよし」ボタンクリック
+*/
+document.getElementById('remove_interest').onclick = function() {
+	removeInterest();
 }
